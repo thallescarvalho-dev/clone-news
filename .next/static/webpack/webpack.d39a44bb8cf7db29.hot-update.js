a083cfe994705f40b65a7a7437b2decaa7440a6c
thallescarvalho-dev <134315654+thallescarvalho-dev@users.noreply.github.com>
2024-04-11 14:24:40 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- .next/static/webpack/webpack.d39a44bb8cf7db29.hot-update.js
+++ .next/static/webpack/webpack.d39a44bb8cf7db29.hot-update.js
@@ -7,12 +7,19 @@
  * or disable the default devtool with "devtool: false".
  * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
  */
-self["webpackHotUpdate_N_E"]("webpack",{},
-/******/ function(__webpack_require__) { // webpackRuntimeModules
-/******/ /* webpack/runtime/getFullHash */
-/******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "24559df5633c0e12"; }
-/******/ }();
-/******/ 
-/******/ }
+self["webpackHotUpdate_N_E"](
+  "webpack",
+  {},
+  /******/ function (__webpack_require__) {
+    // webpackRuntimeModules
+    /******/ /* webpack/runtime/getFullHash */
+    /******/ !(function () {
+      /******/ __webpack_require__.h = function () {
+        return "24559df5633c0e12";
+      };
+      /******/
+    })();
+    /******/
+    /******/
+  },
 );
